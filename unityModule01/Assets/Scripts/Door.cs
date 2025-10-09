0e0965a995b0d924c6819938453c86146cd56267
Dot99 <gui.rocha.felix@gmail.com>
2025-10-09 09:26:47 +0100

    Module04

--- unityModule01/Assets/Scripts/Door.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class Door : MonoBehaviour
-{
-	public void Activate()
-	{
-		Destroy(gameObject);
-	}
-}
--- unityModule03/Assets/Scripts/Base.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class Base : MonoBehaviour
-{
-	public void TakeDamage(int damage)
-	{
-		GameManager.Instance.TakeDamage(-damage);
-	}
-}
